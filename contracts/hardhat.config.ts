3e13981e5e14128db35858a495baaba3b8cb56dd
N <beforeoafterm@gmail.com>
2025-05-26 00:49:33 +0800

    chore: setup localhost and sepolia to hardhat

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-};
+  solidity: '0.8.28',
+  networks: {
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+    },
+    sepolia: {
+      url: process.env.RPC_URL ?? '',
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+}
 
-export default config;
+export default config
